7866c8e932c049075d7926fd0899be79a80077f2
Noriyuki.Namba <ddr43316@kwansei.ac.jp>
2018-10-21 13:48:41 +0900

    about api gateway

--- aws/api_gateway/lambda_function.py
+++ aws/api_gateway/lambda_function.py
@@ -3,6 +3,15 @@ from datetime import datetime
 import decimal
 from boto3.dynamodb.conditions import Key, Attr
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+            return super(DecimalEncoder, self).default(o)
+                                                        
 
 print('Loading function')      # Functionのロードをログに出力
 
